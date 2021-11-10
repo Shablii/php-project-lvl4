95823fcc89846e2dd06f8bdbec85589ae936293e
Vadim Shablii <mefistovad@gmail.com>
2021-11-11 00:51:19 +0200

    fix styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <!-- Styles -->
 <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="https://php-l4-task-manager.herokuapp.com/css/app.css" rel="stylesheet">
 
 </head>
 <body>
