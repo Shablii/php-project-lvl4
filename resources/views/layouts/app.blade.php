b3295b621a714459861a0df3143f45c82e9e5c47
Vadim Shablii <mefistovad@gmail.com>
2021-11-24 01:50:09 +0200

    refactorind deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="csrf-param" content="_token" />
+    <meta name="csrf-param" content="_token">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Менеджер задач</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
